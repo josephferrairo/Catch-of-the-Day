bc9300bba374b8631d151c8e2f20e64f51c049c1
Joseph Ferrairo <josephferrairo@gmail.com>
2017-09-27 23:17:15 -0400

    Set up state for StorePicker

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,12 +2,21 @@ import React from 'react';
 import {getFunName} from '../helpers';
 
 class StorePicker extends React.Component {
+
+  goToStore(event) {
+  event.preventDefault();
+  console.log('You Changed the URL');
+    // first grab the text from the box
+    console.log(this.storeInput.value)
+    // second we're going to transition from / to /store/:storeId
+  }
+
   render() {
     return (
-      <form className="store-selector">
+      <form className="store-selector" onSubmit={(e) => this.goToStore(e)}>
         <h2>Please Enter a Store</h2>
         <input type="text" required placeholder="Store Name"
-        defaultValue={getFunName()} />
+          defaultValue={getFunName()} ref={(input) => { this.storeInput = input}}/>
         <button type="submit"> Visit Store</button>
       </form>
     )
